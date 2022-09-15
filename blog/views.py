cc0aacb125748029708f1856e42f94ef3ff23b74
Said Zareh <saidzareh@gmail.com>
2022-09-15 17:55:20 -0300

    atuzalização 15.09.22

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def chiclete(request):
+
+    return render(request,template_name='blog/home.html')
+
+def jujuba(request):
+    html = '<html><body><h1>Coma Jujuba<h1></boldy></html'
+    return HttpResponse(html)
--- project_site/urls.py
+++ project_site/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import jujuba,chiclete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('chiclete/', chiclete),
+    path('jujuba/', jujuba)
 ]
